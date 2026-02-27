74703d620d037064995fd817470b390fc40e8b7c
Khris Mahilom <kdmahilom@gmail.com>
2026-02-28 07:33:09 +0800

    Implement Accrud Feb2826

---
+++ account-service/src/main/java/com/khrism/corebank/account/Account.java
@@ -0,0 +1,32 @@
+package com.khrism.corebank.account.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String accountNumber;
+
+    @Column(nullable = false)
+    private String customerName;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    private LocalDateTime createdAt;
+}
